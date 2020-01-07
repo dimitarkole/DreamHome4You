7af97069136a277916c7a1eee0716a16f4aca2b4
Dimitar <dim_kolev2002@abv.bg>
2020-01-07 22:01:20 +0200

    creaing BD

---
+++ DreamHouse4You/Data/DreamHouse4You.Data.Models/House.cs
@@ -0,0 +1,37 @@
+﻿namespace DreamHouse4You.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using DreamHouse4You.Data.Common.Models;
+
+    public class House : IAuditInfo, IDeletableEntity
+    {
+        public string Tittle { get; set; }
+
+        public string Description { get; set; }
+
+        public string UploadUserId { get; set; }
+
+        public ApplicationUser UploadUser { get; set; }
+
+        public int Area { get; set; }
+
+        // ToDo: AreaType enum type
+        public string AreaType { get; set; }
+
+        public double Price { get; set; }
+
+        // ToDo: PriceValue enum type
+        public string PriceValue { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ DreamHouse4You/Data/DreamHouse4You.Data.Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿namespace DreamHouse4You.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using DreamHouse4You.Data.Common.Models;
+
+    public class Notification : IAuditInfo, IDeletableEntity
+    {
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
