1477f17afcb59f628b7c8fd8e3dbeb05e66ef202
toncheva_v <toncheva_v@abv.bg>
2020-01-21 13:24:37 +0200

    add service

---
+++ DreamHouse4You/Services/DreamHouse4You.Services/AdminAccount/Access/AddCategoryService.cs
@@ -0,0 +1,19 @@
+﻿namespace DreamHouse4You.Services.AdminAccount.Access
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using DreamHouse4You.Services.Contracts.AdminAccount.Access;
+    
+    //using DreamHouse4You.
+    public class AddCategoryService : IAddCategoryService
+    {
+        /*public ApplicationDbContext
+
+        public AddCategoryService()
+        {
+
+        }*/
+    }
+}
---
+++ DreamHouse4You/Services/DreamHouse4You.Services/Contracts/AdminAccount/Access/IAddCategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace DreamHouse4You.Services.Contracts.AdminAccount.Access
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IAddCategoryService
+    {
+    }
+}
